df65918c8306bbc4e46b983f1cdf0671d291bc9f
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-24 16:24:37 +0000

    Render list

---
+++ src/components/SortableItemContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item-content">
+    <div
+      v-for="(item, index) in indentation"
+      :key="index"
+      class="item-indentation"
+    ></div>
+    <div class="item-wrapper">
+      <div class="item-icon"></div>
+      <div class="item-title">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SortableItemContent',
+  props: {
+    title: String,
+    indentation: Array,
+  },
+};
+</script>
