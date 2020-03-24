eec1410a0fb76d917123459f47bd35890844dedf
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-24 16:24:30 +0000

    Removed fragments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
-import { Plugin } from 'vue-fragment';
 import Buefy from 'buefy';
 import App from './App.vue';
 import 'buefy/dist/buefy.css';
 import './scss/index.scss';
 
 Vue.use(Buefy);
-Vue.use(Plugin);
 
 Vue.config.productionTip = false;
 
