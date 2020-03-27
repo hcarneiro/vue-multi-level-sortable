979774c22aaa3e809546fce541f2fd7f9f78b855
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-27 18:00:59 +0000

    Move items down on drag

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="title is-1">Vue Multi-level Sortable</h1>
     <div class="container main-sortable-container">
       <div class="sortable-title">Sortable items</div>
-      <div class="inner-sortable-container">
+      <div v-el:container class="inner-sortable-container">
         <Sortable-Item :tree="flatData"></Sortable-Itemt>
       </div>
     </div>
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import { findIndex, cloneDeep } from 'lodash';
 import SortableItem from './components/SortableItem';
 
 export default {
@@ -19,6 +20,10 @@ export default {
       flatData: [],
       defaultTop: 24,
       elementTop: 0,
+      pos1: 0,
+      pos2: 0,
+      pos3: 0,
+      element: undefined,
       data: [
         {
           title: 'Header',
@@ -69,6 +74,7 @@ export default {
         };
 
         itemCopy.style = styleObject;
+        itemCopy.top = this.elementTop;
 
         this.flatData.push(itemCopy);
 
@@ -81,6 +87,64 @@ export default {
         }
       });
     },
+    closeDragElement() {
+      /* stop moving when mouse button is released: */
+      document.onmouseup = null;
+      document.onmousemove = null;
+    },
+    elementDrag(evt) {
+      const event = evt || window.event;
+
+      // calculate the new cursor position:
+      this.pos1 = this.pos3 - event.clientX;
+      this.pos2 = this.pos3 - event.clientY;
+      this.pos3 = event.clientY;
+
+      const trans = getComputedStyle(this.element).getPropertyValue('transform');
+      const matrix = trans.replace(/[^0-9\-.,]/g, '').split(',');
+      const ty = parseFloat((matrix.length > 6) ? matrix[13] : matrix[5]);
+
+      // console.log('Horizontal size', this.element.offsetLeft - this.pos1);
+      // element.style.left = `${element.offsetLeft - pos1}px`;
+
+      const containerLimit = {
+        top: 0,
+        bottom: this.$els.container.offsetHeight
+          - this.element.offsetHeight - 1, // 1px of top border
+      };
+
+      const yPosition = Math.min(containerLimit.bottom,
+        Math.max(containerLimit.top,
+          ty + (this.element.offsetTop - this.pos2)));
+
+      this.element.style.transform = `translateY(${yPosition}px)`;
+
+      const itemIndex = findIndex(this.flatData, (itemData) => {
+        return yPosition > itemData.top && yPosition < (itemData.top + this.defaultTop);
+      });
+
+      if (typeof itemIndex === 'undefined') {
+        return;
+      }
+
+      // Move item down
+      const itemClone = cloneDeep(this.flatData[itemIndex]);
+
+      itemClone.style.transform = `translateY(${itemClone.top + this.defaultTop}px)`;
+
+      this.flatData.splice(itemIndex, 1);
+      this.flatData.splice(itemIndex, 0, itemClone);
+    },
+    dragMouseDown(evt) {
+      const event = evt || window.event;
+
+      this.element = event.target;
+
+      // get the mouse cursor position at startup:
+      this.pos3 = event.clientY;
+      document.onmouseup = this.closeDragElement;
+      document.onmousemove = this.elementDrag;
+    },
   },
   created() {
     this.flatten({
@@ -88,11 +152,6 @@ export default {
     });
   },
   ready() {
-    let pos1 = 0;
-    let pos2 = 0;
-    let pos3 = 0;
-    let element;
-
     // function isInViewPort(el) {
     //   const bounding = el.getBoundingClientRect();
 
@@ -105,45 +164,10 @@ export default {
     //   );
     // }
 
-    function closeDragElement() {
-      /* stop moving when mouse button is released: */
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-
-    function elementDrag(evt) {
-      const event = evt || window.event;
-
-      // calculate the new cursor position:
-      pos1 = pos3 - event.clientX;
-      pos2 = pos3 - event.clientY;
-      pos3 = event.clientY;
-
-      const trans = getComputedStyle(element).getPropertyValue('transform');
-      const matrix = trans.replace(/[^0-9\-.,]/g, '').split(',');
-      const ty = parseFloat((matrix.length > 6) ? matrix[13] : matrix[5]);
-
-      console.log('Horizontal size', element.offsetLeft - pos1);
-      // element.style.left = `${element.offsetLeft - pos1}px`;
-
-      element.style.transform = `translateY(${ty + (element.offsetTop - pos2)}px)`;
-    }
-
-    function dragMouseDown(evt) {
-      const event = evt || window.event;
-
-      element = event.target;
-
-      // get the mouse cursor position at startup:
-      pos3 = event.clientY;
-      document.onmouseup = closeDragElement;
-      document.onmousemove = elementDrag;
-    }
-
     const elements = document.querySelectorAll('.sortable-item');
 
     for (let i = 0; i < elements.length; i += 1) {
-      elements[i].onmousedown = dragMouseDown;
+      elements[i].onmousedown = this.dragMouseDown;
     }
   },
 };
